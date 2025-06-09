3ef67aa75387c2df1664ff24e400be89abdf32c8
Laxita2004 <laxitathakur2004@gmail.com>
2025-06-09 17:28:02 +0530

    basic ui setup of shop page

--- src/app/shop/all/page.tsx
+++ src/app/shop/all/page.tsx
@@ -3,11 +3,15 @@
 import React from "react";
 import AllSection from "@/components/shop/AllSection"; // adjust path if needed
 import Header from "@/components/Header";
+import FadeInSection from "@/components/FadeIn";
 
 const AllPage = () => {
   return (
     <div className="mt-20 bg-light">
-        <AllSection />
+        <FadeInSection>
+            <AllSection />
+        </FadeInSection>
+        
     </div>
     
   )
