956240f5aad90238c000c73e1de2806d0c728665
Laxita2004 <laxitathakur2004@gmail.com>
2025-08-03 13:45:19 +0530

    fix 17

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,29 @@
 import "./globals.css";
 import { ReactNode } from "react";
-import { Cormorant_Garamond } from 'next/font/google';
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import { Cormorant_Garamond } from "next/font/google";
 
 const cormorant = Cormorant_Garamond({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  display: 'swap',
-  variable: '--font-cormorant',
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-cormorant",
 });
 
 export const metadata = {
   title: "Adira | The House of Chiffon",
-  description: "Luxury Handcrafted Chiffon Sarees and Rajputi Poshaks.",
+  description:
+    "Adira brings you graceful chiffon sarees where timeless tradition meets quiet luxury. Ethically made, thoughtfully designed, and rooted in Indian elegance.",
+  icons: {
+    icon: "/AdiraLogo.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return ( 
+  return (
     <html lang="en" className={cormorant.variable}>
       <body className="min-h-screen flex flex-col">
-        
-
         {/* Main content grows to fill remaining space */}
         <main className="flex-1">{children}</main>
-
-        
       </body>
     </html>
   );
