37fe9d344d01f2d87b8ee1d27e586dacd90e1f62
Laxita2004 <laxitathakur2004@gmail.com>
2025-08-03 11:44:34 +0530

    fix 11

--- src/app/shop/all/page.tsx
+++ src/app/shop/all/page.tsx
@@ -1,5 +1,7 @@
 // app/shop/all/page.tsx
 "use client"
+export const dynamic = "force-dynamic"; // 🚫 disable prerendering --> quick fix for now to avoid error in deployemnet
+// error was : page getting pre-rendered
 import React from "react";
 import AllSection from "@/components/shop/AllSection"; // adjust path if needed
 import Header from "@/components/Header";
